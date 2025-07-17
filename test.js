5a65e31f035d72184f4c49676b55db7bc61c1c24
Labib Hasan <labibshawon101@gmail.com>
2025-07-17 22:33:43 +0600

    first commit

---
+++ test.js
@@ -0,0 +1,31 @@
+Prb-1
+
+for (let i = 1; i <= 10; i++) {
+  console.log(i);
+}
+
+
+Prb-2
+
+const numbers = [1,2,3,4,5,6,7,8,9,10];
+
+console.log("Even numbers:");
+
+for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] % 2 === 0) {
+        console.log(numbers[i]);
+    }
+}
+
+
+const numbers = [1,2,3,4,5,6,7,8,9,10];
+
+console.log("Odd numbers:");
+
+for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] % 2 !== 0) {
+        console.log(numbers[i]);
+    }
+}
+
+
