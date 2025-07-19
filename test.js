d07634c6504613665343a4f2013044ee853f1151
Labib Hasan <labibshawon101@gmail.com>
2025-07-19 21:43:40 +0600

    new update

--- test.js
+++ test.js
@@ -1,60 +1,77 @@
-Prb-1
+// Prb-1
 
-for (let i = 1; i <= 10; i++) {
-  console.log(i);
-}
-
-
-Prb-2
-
-const numbers = [1,2,3,4,5,6,7,8,9,10];
-
-console.log("Even numbers:");
-
-for (let i = 0; i < numbers.length; i++) {
-    if (numbers[i] % 2 === 0) {
-        console.log(numbers[i]);
-    }
-}
+// for (let i = 1; i <= 10; i++) {
+//   console.log(i);
+// }
 
 
+// Prb-2
 
+// const numbers = [1,2,3,4,5,6,7,8,9,10];
 
-console.log("Odd numbers:");
+// console.log("Even numbers:");
 
-for (let i = 0; i < numbers.length; i++) {
-    if (numbers[i] % 2 !== 0) {
-        console.log(numbers[i]);
-    }
-}
-
-
-Prblm 7 
-
-for (let i = 10; i >= 1; i--) {
-  console.log(i);
-}
-
-
-Prblm 6
-
-
-let sum = 0;
-
-for (let i = 1; i <= 20; i++) {
-  sum += i;
-}
-
-console.log("1-20 er Jogfol :", sum);
-
-// Prblm 4
-
-let number = 7; 
-
-for (let i = 1; i <= 10; i++) {
-    console.log(`${number} * ${i} = ${number * i}`);
-}
+// for (let i = 0; i < numbers.length; i++) {
+//     if (numbers[i] % 2 === 0) {
+//         console.log(numbers[i]);
+//     }
+// }
 
 
 
 
+// console.log("Odd numbers:");
+
+// for (let i = 0; i < numbers.length; i++) {
+//     if (numbers[i] % 2 !== 0) {
+//         console.log(numbers[i]);
+//     }
+// }
+
+
+// Prblm 7 
+
+// for (let i = 10; i >= 1; i--) {
+//   console.log(i);
+// }
+
+
+// Prblm 6
+
+
+// let sum = 0;
+
+// for (let i = 1; i <= 20; i++) {
+//   sum += i;
+// }
+
+// console.log("1-20 er Jogfol :", sum);
+ 
+// // Prblm 4
+
+// let number = 7; 
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log(`${number} * ${i} = ${number * i}`);
+// }
+
+
+
+
+let yourName = "Labib";
+let yourAge = 26;
+let isStudent = true;
+let x = null;
+
+
+console.log("Name:",yourName);
+console.log("Age:",yourAge);
+console.log("Occupation:",isStudent);
+console.log("value:",x);
+
+
+let name ="Labib";
+let Age = 26;
+
+ let message = `Hellow, I am ${name} and I am ${Age} years old`;
+ console.log(message);
